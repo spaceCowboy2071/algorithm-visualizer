e8e93f00a9a6404994737e5b05c18b569727015c
JP <jamespruitt1994@gmail.com>
2026-01-14 21:58:17 -0500

    feat: Add Blind 75 Challenge with expandable problem categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import SortingPage from './pages/SortingPage';
+import Blind75Page from './pages/Blind75Page';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/sorting" element={<SortingPage />} />
+        <Route path="/blind75" element={<Blind75Page />} />
       </Routes>
     </BrowserRouter>
   );
