3d4e30bc022edd07899e954d81f3c256d8d0350f
JP <jamespruitt1994@gmail.com>
2026-01-14 22:21:18 -0500

    feat: Add template for Two Sum to Blind 75 Challenge

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import SortingPage from './pages/SortingPage';
 import Blind75Page from './pages/Blind75Page';
+import ProblemPage from './pages/ProblemPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/sorting" element={<SortingPage />} />
         <Route path="/blind75" element={<Blind75Page />} />
+        <Route path="/blind75/problem/:id" element={<ProblemPage />} />
       </Routes>
     </BrowserRouter>
   );
